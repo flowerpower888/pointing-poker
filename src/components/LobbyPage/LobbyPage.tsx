e1db4faa6e223b4e751c798ff13f795b8885e7e5
Kate <kate.matveeva@gmail.com>
2021-09-07 00:40:38 +0300

    feat: create lobby page

---
+++ src/components/LobbyPage/LobbyPage.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import LobbyPagePlayers from './LobbyPagePlayers';
+
+function lobbyPage(): JSX.Element {
+  return (
+    <div className="lobby-page">
+      <LobbyPagePlayers />
+    </div>
+  );
+}
+export default lobbyPage;
