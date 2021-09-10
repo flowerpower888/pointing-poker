ccdd2d28061af5349f82da28cdc9ef9965330ccb
imyzhuk <imyzhuk@gmail.com>
2021-09-10 10:21:34 +0300

    feat: add index.ts files to components directories

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
-import Footer from './components/Footer/Footer';
+import Header from './components/Header';
+import Home from './components/Home';
+import Footer from './components/Footer';
 
 const App: React.FunctionComponent = () => (
   <>
