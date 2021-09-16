0d97bb1797e5f91938f00284668c83178d9f8d96
Kate <kate.matveeva@gmail.com>
2021-09-16 14:43:30 +0300

    fix: change lobby page data source

--- src/components/LobbyPage/LobbyPage.tsx
+++ src/components/LobbyPage/LobbyPage.tsx
@@ -5,6 +5,7 @@ import './lobbyPage.css';
 import LobbyPageScramMaster from './LobbyPageScramMaster';
 import LobbyPagePlayers from './LobbyPagePlayers';
 import gameAPI from '../../api/gameAPI';
+import { GameInfo } from '../../types/types';
 
 type GameParams = {
   gameId: string;
@@ -13,20 +14,31 @@ type GameParams = {
 function LobbyPage(): JSX.Element {
   const { gameId } = useParams<GameParams>();
   const [isOwner, setIsOwner] = useState(false);
+  const [gameData, setGameData] = useState({} as GameInfo);
+  const [isLoading, setIsLoading] = useState(true);
   // const [gameStatus, setGameStatus] = useState('created');
   useEffect(() => {
     async function getOwnerStatus() {
       const gameInfo = await gameAPI.getGameInfo(gameId);
+      setGameData(gameInfo.data);
       const owner = gameInfo.data.members.filter(el => el.isOwner)[0].id;
       setIsOwner(owner === localStorage.getItem('userId'));
       // setGameStatus(gameInfo.data.status);
+      setIsLoading(false);
     }
     getOwnerStatus();
   }, []);
   // TODO: add checking game status to render lobby or game page
+  if (isLoading) {
+    return <div>loading...</div>;
+  }
   return (
     <div className="lobby-page">
-      {isOwner ? <LobbyPageScramMaster /> : <LobbyPagePlayers />}
+      {isOwner ? (
+        <LobbyPageScramMaster info={gameData} />
+      ) : (
+        <LobbyPagePlayers info={gameData} />
+      )}
     </div>
   );
 }
--- src/components/LobbyPage/LobbyPagePlayers/LobbyPagePlayers.tsx
+++ src/components/LobbyPage/LobbyPagePlayers/LobbyPagePlayers.tsx
@@ -4,27 +4,34 @@ import 'antd/dist/antd.css';
 import './lobbyPagePlayers.css';
 import MembersList from '../Members/MembersList';
 import UserCard from '../UserCard/UserCard';
-// eslint-disable-next-line import/no-named-as-default
-import members from '../ConstantsHardCode';
+import { GameInfo } from '../../../types/types';
 
-function LobbyPagePlayers(): JSX.Element {
+type Game = {
+  info: GameInfo;
+};
+
+function LobbyPagePlayers(props: Game): JSX.Element {
+  const { info } = props;
+  console.log(info);
+  const scrumMaster = info.members.filter(el => el.isOwner)[0];
+  const players = info.members.filter(el => !el.isOwner);
   return (
     <>
       <h2 className="lobby-title"> Spring planning</h2>
       <UserCard
         isOwner
-        imagePath=""
-        firstName="User"
-        lastName="Name"
-        userRole="observer"
-        jobPosition="student"
+        imagePath={scrumMaster.imagePath}
+        firstName={scrumMaster.firstName}
+        lastName={scrumMaster.lastName}
+        userRole={scrumMaster.userRole}
+        jobPosition={scrumMaster.jobPosition}
       />
       <div className="lobby-page_btn-container">
         <Button className="lobby-exit-btn" type="default" size="large">
           Exit
         </Button>
       </div>
-      <MembersList users={members} />
+      <MembersList users={players} />
     </>
   );
 }
