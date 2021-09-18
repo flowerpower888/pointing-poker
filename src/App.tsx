a3e4b949716ac7961ef10cd84dd25cb2246e12bb
imyzhuk <imyzhuk@gmail.com>
2021-09-18 14:20:51 +0300

    feat: add game page to routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,14 @@ import './app.scss';
 import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
-import LobbyPage from './components/LobbyPage';
-import GamePage from './components/GamePage';
+import MainPage from './components/MainPage/MainPage';
 
 const App: React.FunctionComponent = () => (
   <BrowserRouter>
     <Header />
     <Switch>
       <Route path="/:gameId">
-        <LobbyPage />
+        <MainPage />
       </Route>
       <Route exact path="/" render={() => <Home />} />
     </Switch>
---
+++ src/components/MainPage/MainPage.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import gameAPI from '../../api/gameAPI';
+import Preloader from '../common/Preloader/Preloader';
+import {
+  GameInfo,
+  GameStatus,
+} from '../../models/GameInfoAggregate/GameInfoModel';
+import LobbyPage from '../LobbyPage';
+import GamePage from '../GamePage';
+
+type GameParams = {
+  gameId: string;
+};
+
+const MainPage: React.FC = () => {
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const { gameId } = useParams<GameParams>();
+  const [gameData, setGameData] = useState({} as GameInfo);
+  const [gameStatus, setGameStatus] = useState<GameStatus>('created');
+  useEffect(() => {
+    async function getOwnerStatus() {
+      const gameInfo = await gameAPI.getGameInfo(gameId);
+      setGameData(gameInfo.data);
+      setGameStatus(gameInfo.data.status);
+      setIsLoaded(true);
+    }
+
+    getOwnerStatus();
+  }, []);
+
+  return (
+    <div className="lobby-page">
+      {!isLoaded && <Preloader />}
+      {isLoaded && gameStatus === 'created' && <LobbyPage info={gameData} />}
+      {isLoaded && gameStatus === 'started' && <GamePage info={gameData} />}
+    </div>
+  );
+};
+
+export default MainPage;
