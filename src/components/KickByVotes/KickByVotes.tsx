451da3007645ce095e110edf4dab05332b9ce145
Kate <kate.matveeva@gmail.com>
2021-10-04 00:19:56 +0300

    fix: fix kick voting

--- src/components/KickByVotes/KickByVotes.tsx
+++ src/components/KickByVotes/KickByVotes.tsx
@@ -3,47 +3,6 @@ import { Modal } from 'antd';
 import { Member } from '../../models/GameInfoAggregate/GameInfoModel';
 import kickByVoteAPI from '../../api/kickByVoteAPI';
 
-// const { confirm } = Modal;
-//
-// const KickByVotes = (
-//   kickProposeBy: Member,
-//   playerToKick: Member,
-//   showModal: boolean,
-//   setShowModal: React.Dispatch<React.SetStateAction<boolean>>,
-// ): void => {
-//   function sendVote(vote: boolean) {
-//     const gameId = localStorage.getItem('gameId');
-//     if (gameId && playerToKick.id && kickProposeBy.id) {
-//       kickByVoteAPI.sendVotes(
-//         gameId,
-//         playerToKick.id,
-//         kickProposeBy.id,
-//         'voting',
-//         vote,
-//       );
-//     }
-//     setShowModal(false);
-//   }
-//
-//   confirm({
-//     title: `${kickProposeBy.firstName} ${kickProposeBy.lastName} wants to kick member ${playerToKick.firstName} ${playerToKick.lastName}.
-//     Do you agree with it?`,
-//     icon: ExclamationCircleOutlined,
-//     okText: 'Yes',
-//     okType: 'danger',
-//     cancelText: 'No',
-//     visible: showModal,
-//     onOk() {
-//       sendVote(true);
-//     },
-//     onCancel() {
-//       sendVote(false);
-//     },
-//     centered: true,
-//     maskClosable: true,
-//   });
-// };
-
 function KickByVotes(
   kickProposeBy: Member,
   playerToKick: Member,
@@ -70,7 +29,11 @@ function KickByVotes(
   }
   return (
     <Modal
-      title={`${kickProposeByFirstName} ${kickProposeByLastName} wants to kick member ${playerToKickFirstName} ${playerToKickLastName}.
+      title={`${kickProposeByFirstName} ${
+        kickProposeByLastName || ''
+      } wants to kick member ${playerToKickFirstName} ${
+        playerToKickLastName || ''
+      }.
     Do you agree with it?`}
       okText="Yes"
       okType="danger"
